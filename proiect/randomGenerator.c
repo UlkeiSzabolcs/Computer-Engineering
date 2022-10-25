49e1f99a07767fefba783b1aa5b9fbf118870abe
UlkeiSzabolcs <80409839+UlkeiSzabolcs@users.noreply.github.com>
2022-10-25 15:47:55 +0300

    Remove unnecessary random number generation

--- proiect/randomGenerator.c
+++ proiect/randomGenerator.c
@@ -8,7 +8,6 @@ int main(){
     int rand_num;
     srand(time(0));
     for(int i = 0; i < 600; i++){
-        rand_num = rand();
         rand_num = rand() % 200;
         fprintf(f, "%d ", rand_num);
     }
